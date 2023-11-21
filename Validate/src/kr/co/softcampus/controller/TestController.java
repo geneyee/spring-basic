534a83934078a6965365cb1a835d88f8b05fba1e
geneyee <geneyeey@gmail.com>
2023-11-21 13:37:10 +0900

    lamdba 추가

--- Validate/src/kr/co/softcampus/controller/TestController.java
+++ Validate/src/kr/co/softcampus/controller/TestController.java
@@ -1,7 +1,11 @@
 package kr.co.softcampus.controller;
 
+import java.util.Arrays;
+
+import javax.security.auth.x500.X500Principal;
 import javax.validation.Valid;
 
+import org.springframework.remoting.rmi.CodebaseAwareObjectInputStream;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
@@ -31,27 +35,46 @@ public class TestController {
 //		System.out.println("BindResult : " + result);
 		
 		//유효성 검사에서 위반된 부분이 있다면
+//		if(result.hasErrors()) {
+//			//유효성 위반 결과들을 모두 가져온다.
+//			for(ObjectError obj : result.getAllErrors()) {
+//				System.out.println("메시지 : " + obj.getDefaultMessage());
+//				System.out.println("code : " + obj.getCode());
+//				System.out.println("object name : " + obj.getObjectName());
+//				
+//				String[] codes = obj.getCodes();
+//				for(String c1 : codes) {
+//					System.out.println(c1);
+//				}
+//				
+//				if(codes[0].equals("Size.dataBean1.data1")) {
+//					System.out.println("data1은 2 ~ 10글자를 담을 수 있습니다.");
+//				}else if(codes[0].equals("Max.dataBean1.data2")) {
+//					System.out.println("data2는 100이하의 값만 담을 수 있습니다.");
+//				}
+//				
+//				System.out.println("--------------------------------------------");
+//			}
+//			return "input_data";
+//		}
+		
 		if(result.hasErrors()) {
-			//유효성 위반 결과들을 모두 가져온다.
-			for(ObjectError obj : result.getAllErrors()) {
+			
+			result.getAllErrors().forEach(obj -> {
 				System.out.println("메시지 : " + obj.getDefaultMessage());
 				System.out.println("code : " + obj.getCode());
 				System.out.println("object name : " + obj.getObjectName());
-				
-				String[] codes = obj.getCodes();
-				for(String c1 : codes) {
-					System.out.println(c1);
-				}
-				
-				if(codes[0].equals("Size.dataBean1.data1")) {
-					System.out.println("data1은 2 ~ 10글자를 담을 수 있습니다.");
-				}else if(codes[0].equals("Max.dataBean1.data2")) {
-					System.out.println("data2는 100이하의 값만 담을 수 있습니다.");
-				}
-				
-				System.out.println("--------------------------------------------");
-			}
-			
+			});
+
+			result.getAllErrors().stream()
+				.map(ObjectError::getCodes)
+				.forEach(codes -> {
+					if(codes[0].equals("Size.dataBean1.data1")) {
+						System.out.println("data1은 2 ~ 10글자를 담을 수 있습니다.");
+					}else if(codes[0].equals("Max.dataBean1.data2")) {
+						System.out.println("data2는 100이하의 값만 담을 수 있습니다.");
+					}
+				});
 			return "input_data";
 		}
 		
